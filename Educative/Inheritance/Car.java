cd227e05ce551e57e84617e603c0e3cfefb9e389
Gloria Win Nyunt <94699079+gloriabee@users.noreply.github.com>
2023-02-26 19:42:37 +0700

    Inheritance

---
+++ Educative/Inheritance/Car.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+public class Car extends Vehicle{
+    private String bodyStyle;
+
+    public Car(String make, String color, int year, String model, String bodyStyle) {
+        super(make, color, year, model);
+        this.bodyStyle = bodyStyle;
+    }
+
+    public void printDetails(){
+        super.printDetails();
+        System.out.println("Body Style: "+bodyStyle);
+    }
+    
+}
---
+++ Educative/Inheritance/MainTest.java
@@ -0,0 +1,8 @@
+package Inheritance;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Car c1=new Car("Hyundai", "red", 2009, "Elantra", "Sedan");
+        c1.printDetails();
+    }
+}
